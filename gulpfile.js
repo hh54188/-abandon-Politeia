6dea997bbe62677fedd9820746d5fec4180783a9
liguangyi <252438823@qq.com>
2017-04-09 21:10:08 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,48 @@ var gulp = require('gulp');
 var path = require('path');
 var nunjucksRender = require('gulp-nunjucks-render');
 var webserver = require('gulp-connect');
-var less = require('gulp-less')
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
 
-gulp.task('dev', ['compile template', 'dev web server', 'compile less']);
+gulp.task('dev', ['watch', 'compile template', 'dev web server', 'compile less']);
 var outputRootPath = path.join(__dirname, 'releases');
 
-var templateFolderPath = path.join(__dirname, 'src', 'templates');
 var templateOuputFolderPath = path.join(outputRootPath, 'pages');
 gulp.task('compile template', function () {
-    return gulp.src(path.join(templateFolderPath, '*.+(html|nunjucks)'))
+    return gulp.src(path.join(__dirname, 'src', 'pages', '*.nunjucks'))
         .pipe(nunjucksRender({
-            path: [templateFolderPath],
-            watch: true
+            path: [path.join(__dirname, 'src', 'templates')]
         }))
         .pipe(gulp.dest(templateOuputFolderPath));
 });
 
-var webserverRootPath = outputRootPath;
+var webserverRootPath = path.join(__dirname);
 gulp.task('dev web server', function () {
     webserver.server({
-        root: webserverRootPath,
-        livereload: true
+        root: webserverRootPath
     });
 });
 
-var lessDirPath = path.join(__dirname, 'src', 'styles');
 var styleOutputDirPath = path.join(outputRootPath, 'styles');
 gulp.task('compile less', function () {
-    return gulp.src(path.join(lessDirPath, '*.less'))
+    return gulp.src(path.join(__dirname, 'src', 'styles', 'main', '*.less'))
         .pipe(less({
-            paths: [lessDirPath]
+            paths: [path.join(__dirname, 'src', 'styles', 'includes')]
         }))
         .pipe(gulp.dest(styleOutputDirPath));
 });
+
+var templatesDirs = [path.join(__dirname, 'src', 'templates', '**', '*.nunjucks'), path.join(__dirname, 'src', 'pages', '*.nunjucks')];
+var lessDirs = [path.join(__dirname, 'src', 'styles', '**', '*.less')];
+gulp.task('watch', function () {
+    gulp.watch(templatesDirs, ['compile template']);
+    gulp.watch(lessDirs, ['compile less']);
+});
+
+// gulp.task('concat css', function () {
+//     return gulp.src('./releases/styles/index.css')
+//         .pipe(concatCss('./normalize.css'))
+//         .pipe(gulp.dest(styleOutputDirPath));
+// });
 
