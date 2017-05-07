99c5f63c2e2ee88e4a97ddab1210aae1670a73e3
liguangyi <252438823@qq.com>
2017-05-08 00:14:07 +0800

    update

--- server.js
+++ server.js
@@ -14,11 +14,17 @@ app.get('/api/ebook/books', (req, res) => {
     }, 1000 * 2);
 });
 
+app.get('/api/ebook/read_history', (req, res) => {
+    setTimeout(() => {
+        res.send(fs.readFileSync('./mock_data/books.json', 'utf-8'));
+    }, 1000 * 2);
+});
+
 app.get('/api/ebook/reviews', (req, res) => {
     setTimeout(() => {
         res.send(fs.readFileSync('./mock_data/comments.json', 'utf-8'))
     }, 1000 * 2);
-})
+});
 
 app.listen(8000);
 console.log('Server Start: http://127.0.0.1:8000');
